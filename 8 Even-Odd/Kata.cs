6e97efbe453142e9cdd76f5c170fdef1f3216b23
Martin Cervantes <cervantes.martine@gmail.com>
2018-04-10 16:24:20 -0600

    even-odd

---
+++ 8 Even-Odd/Kata.cs
@@ -0,0 +1,10 @@
+public class SolutionClass
+{
+    public static string EvenOrOdd(int number)
+    {
+        if(number % 2 == 0)
+            return "Even";
+        else
+            return "Odd";
+    }
+}
---
+++ 8 Even-Odd/Kata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+number=$1
+res=$number%2
+
+if [[ $res -eq 0 ]]
+then
+  echo "Even"
+else
+  echo "Odd"
+fi
