50f98a3c5981f93cca22f91eeeb71fd775696230
Martin Cervantes <cervantes.martine@gmail.com>
2018-04-12 16:25:40 -0600

    add 5

---
+++ 8 Add 5/Kata.cs
@@ -0,0 +1,8 @@
+public class Kata
+{
+    public static int AddFive(int num)
+    {
+        int total = num + 5;
+        return total;
+    }
+}
---
+++ 8 Add 5/Kata.java
@@ -0,0 +1,8 @@
+public class Solution
+{
+    public static int addFive(int num)
+    {
+        int total = num + 5;
+        return total;
+    }
+}
