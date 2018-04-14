a5f5c397e199f30cb1cf128b9e528828c182c39b
Martin Cervantes <cervantes.martine@gmail.com>
2018-04-14 16:26:48 -0600

    secret message

---
+++ 8 Secret Message/Kata.cpp
@@ -0,0 +1,7 @@
+std::string greet(std::string name)
+{
+    if(name == "Johnny")
+        return "Hello, my love!";
+    else
+        return "Hello, " + name + "!";
+}
---
+++ 8 Secret Message/Kata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ "$1" = "Johnny" ]]
+then
+    echo "Hello, my Love!";
+else
+    echo "Hello, $1!"
+fi
