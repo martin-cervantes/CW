52cc1fc0c4c76c94c0023a18f5951e3dc73aa5df
Martin Cervantes <cervantes.martine@gmail.com>
2018-04-17 16:28:11 -0600

    parase 2

---
+++ 8 Parse number to string/Kata.cs
@@ -0,0 +1,5 @@
+public class Kata
+{
+    public static int num = 123;
+    public static string A = num.ToString();
+}
---
+++ 8 Parse number to string/Kata.java
@@ -0,0 +1,5 @@
+public class ToString
+{
+    public static final int num = 123;
+    public static final String a = Integer.toString(num);
+}
