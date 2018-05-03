f77830fea9094e034c958e66984b5ce164dfb7a4
Martin Cervantes <cervantes.martine@gmail.com>
2018-05-03 10:29:07 -0600

    basic ops py

---
+++ 8 Basic Math Ops/Kata.py
@@ -0,0 +1,11 @@
+def basic_op(op, val1, val2):
+    if op == '+':
+        return val1 + val2
+    elif op == '-':
+        return val1 - val2
+    elif op == '*':
+        return val1 * val2
+    elif op == '/':
+        return val1 / val2
+    else:
+        return 0
