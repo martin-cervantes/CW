275fe4b49a8db9b3808d8466bf81a82093cce7d0
Martin Cervantes <cervantes.martine@gmail.com>
2018-05-22 18:01:22 -0600

    mymbling py

--- 7 Mumbling/Kata.py
+++ 7 Mumbling/Kata.py
@@ -1,16 +1,21 @@
+import string
+
 def accum(s):
     s = list(s)
-	cadena = ""
+    cadena = ""
+    for x in range(0, len(s) + 1):
+        print(x)
 
-    for i in range(0,len(s)):
+    for i in range(0, len(s) + 1):
         if i != 0:
-            cadena += "-";
+            cadena += str('-');
 
-        for j in range( 0, i):
-            if j == 0:
+        for j in range( 1, i + 1):
+            if j == 1:
                 cadena += s[i].upper()
             else:
                 cadena += s[i].lower()
 
-
     return cadena
+
+print(accum("aHguytT"))
