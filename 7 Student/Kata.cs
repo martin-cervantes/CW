c44f3b3c93fd7fd949a43944aa1139b15a3e4fd8
Martin Cervantes <cervantes.martine@gmail.com>
2018-06-03 10:48:52 -0600

    student 2

---
+++ 7 Student/Kata.cs
@@ -0,0 +1,37 @@
+class Kata
+{
+    public float average(float[] numbers)
+    {
+        float total = 0;
+
+        for(int i = 0; i < numbers.length; i++)
+            total += numbers[i];
+
+        total = total / numbers.length;
+
+        return total;
+    }
+
+    public float get_average(float[] student)
+    {
+        float homework = average(student["homework"]);
+        float quizzes = average(student["quizzes"]);
+        float tests = average(student["tests"]);
+
+        return homework * 0.1 + quizzes * 0.3 + tests * 0.6;
+    }
+
+    public String get_letter_grade(float score)
+    {
+        if(score >= 90)
+            return "A";
+        else if(score >= 80)
+            return "B";
+        else if(score >= 70)
+            return "C";
+        else if(score >= 60)
+            return "D";
+        else
+            return "F";
+    }
+}
